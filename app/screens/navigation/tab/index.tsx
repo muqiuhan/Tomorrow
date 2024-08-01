9e0d82a01b76825e00078ccfcd7fa9c5e8f2f222
Muqiu Han <muqiu-han@outlook.com>
2024-08-01 10:42:43 +0800

    [interface]: add TabNavigation implementation

--- app/screens/navigation/tab/index.tsx
+++ app/screens/navigation/tab/index.tsx
@@ -1,17 +1,63 @@
 import { View, Text } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+import * as ReactNavigationBottomTabs from "@react-navigation/bottom-tabs";
+import * as ReactNavigationStack from "@react-navigation/stack";
+import HomeNavigation from "./HomeNavigation";
+import MarketNavigation from "./MarketNavigation";
+import NewsDetailsScreen from "../../stacks/NewsDetailsScreen";
+import SearchNavigation from "./SearchNavigation";
+import ProfileNavigation from "./ProfileNavigation";
+
+const Tab = ReactNavigationBottomTabs.createBottomTabNavigator();
 
 const TabNavigation = () => {
   return (
-    <View className="flex-1 bg-blue-500 justify-center items-center">
-      <Text
-        className="text-white text-2xl"
-        style={{
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarIcon: ({ focused }) => {
+          const iconName = () => {
+            if (route.name === "home") {
+              return "home";
+            } else if (route.name === "market") {
+              return "stats-chart-outline";
+            } else if (route.name === "search") {
+              return "search-outline";
+            } else if (route.name === "news") {
+              return "newspaper-outline";
+            } else if (route.name === "profile") {
+              return "person-outline";
+            }
+          };
+
+          return (
+            <Ionicons
+              name={iconName()}
+              size={25}
+              color={focused ? "#4984dc" : "gray"}
+            />
+          );
+        },
+        tabBarActiveTintColor: "#4984dc",
+        tabBarInactiveTintColor: "gray",
+        tabBarLabelStyle: {
+          fontSize: 12,
           fontFamily: "MiSans",
-        }}
-      >
-        微光是弱质{" "}
-      </Text>
-    </View>
+          fontWeight: "bold",
+        },
+        ...ReactNavigationStack.TransitionPresets.ScaleFromCenterAndroid,
+        animationEnabled: true,
+        gestureEnabled: true,
+        gestureDirection: "horizontal",
+      })}
+    >
+      <Tab.Screen name="home" component={HomeNavigation} />
+      <Tab.Screen name="market" component={MarketNavigation} />
+      <Tab.Screen name="search" component={SearchNavigation} />
+      <Tab.Screen name="news" component={NewsDetailsScreen} />
+      <Tab.Screen name="profile" component={ProfileNavigation} />
+    </Tab.Navigator>
   );
 };
 
